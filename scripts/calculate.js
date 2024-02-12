b4f001a2965d5fbb0847b62500906d695c291e24
arifulislamk <ariful18374@gmail.com>
2024-02-12 21:50:44 +0600

    added triangle calculationjs

--- scripts/calculate.js
+++ scripts/calculate.js
@@ -1,3 +1,17 @@
-function calculatetriangle(){
-    console.log('conected inside a function')
+function ractanglecalculte(){
+    // get base 
+    const baseInput = document.getElementById('base-input');
+    const baseText = baseInput.value ;
+    const base = parseFloat(baseText)
+    console.log(base)
+    // get height 
+    const heightInput = document.getElementById('height-input');
+    const heightText = heightInput.value ;
+    const height = parseFloat(heightText);
+    console.log(height);
+    // calculate part 
+    const areaInput = document.getElementById('area-text');
+    const area = 0.5 * base * height ;
+    areaInput.innerText = area ;
+    console.log(area)
 }
