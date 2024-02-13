339671ab8a917bbd370042b2d07e9963c519345c
arifulislamk <ariful18374@gmail.com>
2024-02-13 14:35:25 +0600

    ellipse calculation js file

---
+++ scripts/ellipseCal.js
@@ -0,0 +1,6 @@
+function ellipseCalculation(){
+    const eliAaxis = calculateByid('eli-aaxis');
+    const eliBaxis = calculateByid('eli-baxis');
+    const area = 3.1416 * eliAaxis * eliBaxis ;
+    const setAreaEli = setArea('eli-area', area);
+}
