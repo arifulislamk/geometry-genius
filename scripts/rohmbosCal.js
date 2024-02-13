a4e75ea0f0bd26c8fe2136d41fc82f818d67b466
arifulislamk <ariful18374@gmail.com>
2024-02-13 14:11:10 +0600

    rohmbus calculation and degin

---
+++ scripts/rohmbosCal.js
@@ -0,0 +1,6 @@
+function rohmbusCalculation(){
+    const rohmD1 = calculateByid('rom-d1') ;
+    const rohmD2 = calculateByid('rom-d2') ;
+    const area = 0.5 * rohmD1 * rohmD2 ;
+    const setrohmarea = setArea('rohm-area', area)
+}
